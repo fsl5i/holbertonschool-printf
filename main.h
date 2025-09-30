61dc866e2fbaa288cb1161dc3c980672fd9ded84
root <root@ip-10-42-138-76.ec2.internal>
2025-09-30 14:50:46 +0000

    fix: edite main.h file

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdarg.h>
+#include <unistd.h>
 int _printf(const char *format, ...);
+int handle_specifier(va_list *ap, char sp);
 int print_char(char c);
-int print_string(char *s);
+int print_string(const char *s);
 
-#endif /* MAIN_H */
+#endif
