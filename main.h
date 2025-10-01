3e2746ffcc277ea6c91bb3abce8fabb03c6018ba
AleidBanan <Aleidbanan@gmail.com>
2025-10-01 09:24:36 +0000

    printf functoin for integers , task 1

--- main.h
+++ main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <unistd.h>
+
 int _printf(const char *format, ...);
 int handle_specifier(va_list *ap, char sp);
 int print_char(char c);
 int print_string(const char *s);
 int print_int(int n);
+
 #endif
