20409b0e63c6cbdb67659248ca7ee7756a81bb72
AleidBanan <Aleidbanan@gmail.com>
2025-10-01 09:42:20 +0000

    printf functoin for integers , task 1

---
+++ printf_int.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* print_int - prints an integer to stdout
+* @n: the integer to print
+* Return: printed numbers
+*/
+
+int print_int(int n)
+
+{
+
+	int count = 0;
+	char c;
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		count++;
+		n = -n;
+	}
+
+	if (n >= 10)
+		count += print_int(n / 10);
+
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+	count++;
+
+	return count;
+}
