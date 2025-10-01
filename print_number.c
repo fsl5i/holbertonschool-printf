d1d9b3f1d0b23930b1ec2ceed54f813f9a740d81
AleidBanan <Aleidbanan@gmail.com>
2025-10-01 15:37:34 +0000

    printf numbers task 1

--- print_number.c
+++ print_number.c
@@ -1,17 +1,17 @@
 #include <unistd.h>
-#include "main.h"
+
 /**
-* print_int - prints an integer to stdout
-* @n: the integer to print
-* Return: printed numbers
-*/
-
-int print_int(int n)
-
+ * print_number - prints an integer
+ * @n: integer to print
+ *
+ * Return: number of characters printed
+ */
+int print_number(int n)
 {
-
 	int count = 0;
+
 	char c;
+
 
 	if (n < 0)
 	{
@@ -20,12 +20,12 @@ int print_int(int n)
 		n = -n;
 	}
 
-        if (n >= 10)
-		count += print_int(n / 10);
+	if (n >= 10)
+		count += print_number(n / 10);
 
 	c = (n % 10) + '0';
 	write(1, &c, 1);
 	count++;
 
-	return count;
+	return (count);
 }
