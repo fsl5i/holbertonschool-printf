81691caf6b426bc679f990521c92ebb70351cdbb
root <root@ip-10-42-224-146.ec2.internal>
2025-10-02 10:34:16 +0000

    Add main.c test file for _printf

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+int len;
+
+len = _printf("Number: %d\n", 123);
+_printf("Length: %d\n", len);
+
+len = _printf("Negative: %i\n", -456);
+_printf("Length: %d\n", len);
+
+return (0);
+}
