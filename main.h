730cff98876db3485f1a14a407a2b7a64e615d49
root <root@ip-10-42-134-254.ec2.internal>
2025-10-03 20:05:10 +0000

    Update main.h with prototype for print_number_base

--- main.h
+++ main.h
@@ -9,5 +9,6 @@ int print_char(char c);
 int print_string(const char *s);
 int print_int(int n);
 int print_binary(unsigned int n);
+int print_number_base(unsigned int n, int base, int uppercase);
 
 #endif /* MAIN_H */
