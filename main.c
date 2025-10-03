ba5c68d8220f80929c9daf41e371b49b0b681c95
root <root@ip-10-42-134-254.ec2.internal>
2025-10-03 20:05:10 +0000

    Add tests for %u, %o, %x, %X specifiers

--- main.c
+++ main.c
@@ -3,16 +3,28 @@
 
 int main(void)
 {
-int len;
+    int len;
 
-len = _printf("Number: %d\n", 123);
-_printf("Length: %d\n", len);
-
-len = _printf("Negative: %i\n", -456);
-_printf("Length: %d\n", len);
-
-len = _printf("Binary of %d is %b\n", 98, 98);
+    len = _printf("Number: %d\n", 123);
     _printf("Length: %d\n", len);
-    
-return (0);
+
+    len = _printf("Negative: %i\n", -456);
+    _printf("Length: %d\n", len);
+
+    len = _printf("Binary of %d is %b\n", 98, 98);
+    _printf("Length: %d\n", len);
+
+    len = _printf("Unsigned: %u\n", 3000000000U);
+    _printf("Length: %d\n", len);
+
+    len = _printf("Octal: %o\n", 12345);
+    _printf("Length: %d\n", len);
+
+    len = _printf("Hex lowercase: %x\n", 12345);
+    _printf("Length: %d\n", len);
+
+    len = _printf("Hex UPPERCASE: %X\n", 12345);
+    _printf("Length: %d\n", len);
+
+    return (0);
 }
